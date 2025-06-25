5b6543ad406e3599cdba3444585f5dce751fc18c
mmdgitraisoni <160129576+mmdgitraisoni@users.noreply.github.com>
2025-06-25 11:55:31 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+        return user_id
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
